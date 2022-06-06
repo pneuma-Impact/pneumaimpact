0460b489d111f8707a8c70d79b03894d77dff619
Precious Ibeagi <preciousaang@gmail.com>
2022-06-06 10:15:32 +0100

    removed demo process.env.DB_URL

--- core/db.js
+++ core/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const DB_URL = process.env.DB_URL || "mongodb://localhost:27017/pneumaimpact";
+const DB_URL = process.env.DB_URL;
 const slug = require("mongoose-slug-generator");
 mongoose.connect(DB_URL);
 
