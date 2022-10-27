5ccfad9a1336c2482d26d3e5a48d45512e517d94
Precious Ibeagi <preciousaang@gmail.com>
2022-10-27 23:14:37 +0100

    test

--- services/mail.js
+++ services/mail.js
@@ -1,14 +1,14 @@
 const mailgun = require("mailgun-js");
 
 const mg = mailgun({
-  apiKey: process.env.MAILGUN_API_KEY,
-  domain: process.env.DOMAIN,
+  apiKey: "key-1371e9a62a61cd609309d0a69edd9431",
+  domain: "api.pneumaimpact.ng",
 });
 
 exports.sendVerificationMail = async (user) => {
   console.log(user.email);
   const data = {
-    from: `Pneumaimpact <no-reply@${process.env.DOMAIN}>`,
+    from: `Pneumaimpact <no-reply@api.pneumaimpact.ng>`,
     subject: "Verify your email",
     to: `${user.email}`,
     text: `Verify you account using the following code.<br/> <b>${user.verification_token}</b>`,
