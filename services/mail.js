6054893158de9d5cf96a1c6f8ba5581b0251eaf8
Precious Ibeagi <preciousaang@gmail.com>
2022-10-30 17:20:35 +0100

    asd

--- services/mail.js
+++ services/mail.js
@@ -2,7 +2,6 @@ const mailgun = require("mailgun-js");
 
 const mg = mailgun({
   apiKey: process.env.MAILGUN_API_KEY,
-  domain: process.env.DOMAIN,
 });
 
 exports.sendVerificationMail = async (user) => {
